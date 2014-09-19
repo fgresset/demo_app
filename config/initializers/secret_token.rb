f7ee4aaeaa1251a818cf96219d75cba9accecce1
Frederic Gresset <frederic.gresset@telecom-bretagne.eu>
2014-09-19 17:26:43 +0200

    le premier commit de ma life

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2582f2afada0bdcb0560ac9915245cfa2d6ae00c160ec71423b170386c9e7002df2eb1835cb8fcc826edbc86b6137a5aae90b09a54a14200f85d2b35e36eae74'
